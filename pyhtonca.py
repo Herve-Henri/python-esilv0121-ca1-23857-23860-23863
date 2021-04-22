f8cf311ac4e5631a4cdd4a7611cb5ae4e3290a75
lesoldatRyanDS95 <64417756+lesoldatRyanDS95@users.noreply.github.com>
2021-04-22 23:25:51 +0100

    class creation

--- pyhtonca.py
+++ pyhtonca.py
@@ -24,10 +24,59 @@ def CostumerLogin():
 def EmployeeLogin():
     print("nothing for now")
 
+class Person:
 
+    def __init__(self, firstname, lastname, email):
+        self.first_name = firstname
+        self.last_name = lastname
+        self.email = email
+    
+    def printname(self):
+        print(self.firstname, self.lastname, self.email)
+
+    def login_person(self):
+
+
+class Employee(Person):
+    def __init__(self, firstname, lastname, email):
+        Person.__init__(self, firstname, lastname, email)
+        #super().__init__(self, firstname, lastname, email) 
+            #fonction qui fera en sorte que la classe enfant 
+            #hérite de toutes les méthodes et propriétés de son parent
+    
+    def creation_customer(self):
+    
+    def deletion_customer(self):
+    
+    def saving_customer(self):
+
+    def saving_account(self):
+    
+    def creation_transaction(self):
+        #lodge
+        #deposit
+        def add_account(self):
+        def withdraw_account(self):
+
+    def show_customer(self):  
+    
+class Customer(Person):
+    def __init__(self, firstname, lastname, email):
+        Person.__init__(self, firstname, lastname, email)
+        #super().__init__(self, firstname, lastname, email) 
+            #fonction qui fera en sorte que la classe enfant 
+            #hérite de toutes les méthodes et propriétés de son parent
+    
+    def retrieve_transaction(self):
+    def add_money(self):
+    def subtract_money(self):
+    
+   
 
 
 def main():
+    x = Employee("Ryan","DOS SANTOS","23860@student.dorset-college.ie")
+    x.printname()
     menu()
 
 
