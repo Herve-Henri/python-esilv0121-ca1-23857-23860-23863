f5624285a18d0575afbfe7b519d5ddd56a232caa
Hervé-Henri <23857@student.dorset-college.ie>
2021-04-22 16:53:27 +0100

    first draft

--- pyhtonca.py
+++ pyhtonca.py
@@ -1,1 +1,34 @@
-test
+#23857 Hervé-Henri HOUZARD 
+#23860 Ryan DOS SANTOS
+#23863 Benjamin CONSTANT
+
+def menu():
+    print('****************Welcome to Dorset-Bank****************')
+    print('***Please specify whether you are a user or employee**')
+    choice=input("""
+    
+                  1:Costumer
+                  2:Employee   
+    """)
+    if choice=="1":
+        CostumerLogin()
+    elif choice=="2":
+        EmployeeLogin()
+    else:
+        print("You must enter either 1 or 2. \nPlease try again")
+        menu()
+
+def CostumerLogin():
+    print("nothing for now")
+
+def EmployeeLogin():
+    print("nothing for now")
+
+
+
+
+def main():
+    menu()
+
+
+main()
